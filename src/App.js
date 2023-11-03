c69dd571047368d7bfc0d0c14413045e54b1e6e0
rikai-ngoc-le <ngoc.le@rikai.technology>
2023-11-03 13:49:40 +0700

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <p> chào cả lò</p>
+    </div>
+  );
+}
+
+export default App;
